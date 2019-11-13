f0a8e06b71eb6f03fcb383c2230663346476fb55
Gabriel Aniora <chybesta@gmail.com>
2019-11-13 11:13:33 +0100

    setup search box

--- components/Search/Search.js
+++ components/Search/Search.js
@@ -1,19 +1,112 @@
 import React from 'react';
+import { ApolloConsumer } from 'react-apollo';
+import gql from 'graphql-tag';
+import Downshift, { resetIdCounter } from 'downshift';
+import Router from 'next/router';
+import debounce from 'lodash.debounce';
+import  { DropDown, DropDownItem, SearchStyles } from './SearchStyles';
+import formatMoney from '../../lib/formatMoney';
 
-import SearchStyles from './SearchStyles';
+const SEARCH_ITEMS_QUERY = gql`
+  query SEARCH_ITEMS_QUERY($searchTerm: String!) {
+    searchItems(where: { 
+      itemName_contains: $searchTerm
+    }) {
+      id 
+      image1 
+      itemName
+      newPrice
+    }
+  }
+`;
 
-const Search = () => {
+function routeToItem(item) {
+  Router.push({
+    pathname: '/item', 
+    query: {
+      id: item.id,
+    },
+  });
+}
 
-  return (
-    <SearchStyles>
-      <input 
-        type="search" 
-        name="search" 
-        autoFocus 
-        placeholder="What do you want to buy?"
-      />
-    </SearchStyles>
-  )
+class Search extends React.Component {
+  state = {
+    items: [],
+    loading: false,
+  }
+
+  onChange = debounce(async (e, client) => {
+    let keyword = e.target.value.trim();
+  
+    const res = await client.query({
+      query: SEARCH_ITEMS_QUERY,
+      variables: { searchTerm: keyword },
+    });
+
+    this.setState({
+      items: res.data.searchItems,
+    });
+
+  }, 100);
+
+  render() {
+    resetIdCounter();
+    return (
+      <SearchStyles>
+        <Downshift onChange={routeToItem} itemToString={item => (item === null ? '' : '')}>
+          {
+            ({ getInputProps, getItemProps, isOpen, inputValue, highlightedIndex}) => (
+                <div>
+                  <ApolloConsumer>
+                    {
+                      (client) => (
+                        <input 
+                          {...getInputProps({ 
+                            type: "search",
+                            placeholder: "Search here...",
+                            className: this.state.loading ? 'loading': '',
+                            onChange: e => {
+                                e.persist();
+                                this.onChange(e, client);
+                              }
+                            })
+                          }
+                        />
+                      )
+                    }
+                  </ApolloConsumer>
+                  {
+                    isOpen && (
+                      <DropDown>
+                        {this.state.items.map((item, index) => 
+                          <DropDownItem 
+                            {...getItemProps({ item })} 
+                            key={item.id}
+                            highlighted={index === highlightedIndex}
+                            >
+                            <div className="img-text">
+                              <img width="50" src={item.image1} alt={item.itemName} />
+                              <span>{item.itemName}</span>
+                            </div>
+                            <span>{formatMoney(item.newPrice)}</span>
+                          </DropDownItem>)}
+                          {
+                            !this.state.items.length && !this.state.loading && (
+                              <DropDownItem>
+                                Nothing Found for {inputValue}
+                              </DropDownItem>
+                            )
+                          }
+                      </DropDown>
+                    )
+                  }
+                </div>
+            )
+          }
+        </Downshift>
+      </SearchStyles>
+    )
+  }
 }
 
 export default Search;
--- components/Search/SearchStyles.js
+++ components/Search/SearchStyles.js
@@ -1,14 +1,42 @@
 import styled from 'styled-components';
 
+const DropDown = styled.div`
+  position: absolute;
+  width: 300px;
+  z-index: 2;
+`;
+
+const DropDownItem = styled.div`
+  border-bottom: 1px solid teal;
+  background: white;
+  padding: 1rem;
+  transition: all 0.2s;
+  ${props => (props.highlighted ? 'padding-left: 2rem;' : null)};
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  border-left: 10px solid white;
+  &:hover {
+    cursor: pointer;
+  }
+  img {
+    margin-right: 10px;
+  }
+  .img-text {
+    display: flex;
+    align-items: center;
+  }
+`;
+
 const SearchStyles = styled.div`
     display: flex;
-    flex: 4;
+    flex: 1;
     justify-content: center;
     align-items: center;
 
   input[type=search] {
     display: flex;
-    flex: 1;
+    width: 300px;
     padding: 10px;
     font-size: 17px;
     border: 0px;
@@ -21,4 +49,4 @@ const SearchStyles = styled.div`
   }
 `;
 
-export default SearchStyles;
+export { DropDown, DropDownItem, SearchStyles };
